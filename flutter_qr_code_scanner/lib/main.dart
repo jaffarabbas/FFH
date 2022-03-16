2966c72558896f2293176d3ad29f1111dd9976fa
Jaffar Abbas <gamaportal8@gmail.com>
2022-03-16 11:32:38 +0500

    16/03/2022 11:32:30

---
+++ flutter_qr_code_scanner/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qr_code_scanner/Screens/home._screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
